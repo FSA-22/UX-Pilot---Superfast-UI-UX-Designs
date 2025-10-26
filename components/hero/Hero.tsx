424bdcf48e18cfee465cc18230a89752362688d5
FSA-22 <simeonstat@gmail.com>
2025-10-26 02:31:32 +0000

    make hero animation section responsive add avatar-2 to public image   folder

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -6,11 +6,11 @@ import { Button } from '@/components/ui/button';
 
 const Hero = () => {
   return (
-    <section className="flex flex-col md:flex-row items-center justify-between bg-herobg gap-10 md:gap-16 px-6 md:px-16 py-20 bg-gray-50 text-gray-500 m-h-screen h-screen w-full  overflow-hidden">
+    <section className="flex flex-col md:flex-row items-center justify-between bg-herobg gap-10 max-md:py-6 max-md:mb-5 md:gap-16 px-6 md:px-16 py-20 bg-gray-50 text-gray-500 m-h-screen h-screen w-full  overflow-hidden">
       {/* LEFT SIDE */}
       <div className="flex flex-col items-start text-left max-w-xl space-y-6">
-        <h1 className="text-4xl md:text-6xl font-extrabold text-black leading-tight">
-          Automussh&nbsp;
+        <h1 className="text-5xl md:text-6xl font-extrabold text-black leading-tight">
+          Automussh{' '}
           <span className="text-transparent bg-clip-text bg-linear-to-r from-darkblue to-lightblue">
             Insight
           </span>
@@ -20,11 +20,13 @@ const Hero = () => {
           human-centered insights.
         </p>
 
-        <div className="flex flex-wrap gap-4 mt-4">
-          <Button className="primary-button h-15 w-40">Get Started</Button>
+        <div className="flex flex-wrap max-md:flex-col gap-4  max-md:w-full mt-4">
+          <Button className="primary-button h-15 w-40 max-md:w-full">
+            Get Started
+          </Button>
           <Button
             variant="outline"
-            className="border-darkblue border-2 text-lg text-darkblue h-15 w-40 rounded-2xl hover:bg-darkblue hover:text-white transition"
+            className="border-darkblue border-2 text-lg max-md:w-full text-darkblue h-15 w-40 rounded-2xl hover:bg-darkblue hover:text-white transition"
           >
             Learn More
           </Button>
@@ -36,7 +38,7 @@ const Hero = () => {
         {/* Dashboard Image */}
         <div className="relative w-[320px] md:w-[500px]">
           <motion.div
-            className="absolute -top-5 right-26 w-8 h-8 rounded-full bg-lightblue shadow-sm"
+            className="absolute -top-5 right-26 max-md:-right-3 w-8 h-8 rounded-full bg-lightblue shadow-sm"
             animate={{ opacity: [0.2, 1, 0.2] }}
             transition={{ duration: 2, repeat: Infinity, ease: 'easeInOut' }}
           />
